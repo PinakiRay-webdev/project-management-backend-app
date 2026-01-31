1f7d1676674f3a265af3891ab1e0a7ee27479a5b
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2026-01-31 18:45:16 +0530

    removed the api guard

--- gateway/app.js
+++ gateway/app.js
@@ -1,18 +1,12 @@
 import express from "express"
 import expressProxy from "express-http-proxy"
-import authGuard from "./middleware/authGuard.js";
 import cookieParser from "cookie-parser";
 
 const app = express()
 
 app.use(cookieParser())
 app.use('/api/auth', expressProxy('http://localhost:5001'))
-app.use('/api/workspaces', authGuard, expressProxy('http://localhost:5002', {
-    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
-        proxyReqOpts.headers['x-user-id'] = srcReq.user._id;
-        return proxyReqOpts;
-    }
-}));
+app.use('/api/workspaces', expressProxy('http://localhost:5002'))
 
 
 export default app  
--- workspace/controller/workspace.controller.js
+++ workspace/controller/workspace.controller.js
@@ -2,11 +2,10 @@ import workspaceDB from "../model/workspace.model.js";
 import {v4 as uuidv4} from "uuid"
 
 export const createWorkSpace = async (req,res) =>{
-    const ownerId = req.headers['x-user-id']
     try {
-        const {name,description, member} = req.body
+        const {name,description, ownerId, member} = req.body
 
-        const newWorkspace = await new workspaceDB({name, description, inviteCode: uuidv4(), member:[...member, {userId: ownerId, role: 'admin'}]})
+        const newWorkspace = await new workspaceDB({name, description, ownerId, inviteCode: uuidv4(), member:[...member, {userId: ownerId, role: 'admin'}]})
         
         newWorkspace.save()
 
