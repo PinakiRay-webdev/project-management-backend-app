2765e45b45e4bab55b6e39fb8507f6141e26db95
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2026-02-02 17:01:25 +0530

    added the cors policy

--- gateway/app.js
+++ gateway/app.js
@@ -1,9 +1,11 @@
 import express from "express"
 import expressProxy from "express-http-proxy"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express()
 
+app.use(cors())
 app.use(cookieParser())
 app.use('/api/auth', expressProxy('http://localhost:5001'))
 app.use('/api/workspaces', expressProxy('http://localhost:5002'))
