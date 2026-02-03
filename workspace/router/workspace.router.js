da974508e80477f69740c454e1894c95bc6ead4e
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2026-02-03 10:34:20 +0530

    added a router to display all the workspaces

--- workspace/router/workspace.router.js
+++ workspace/router/workspace.router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createWorkSpace } from "../controller/workspace.controller.js";
+import { createWorkSpace, displayAllWorkspace } from "../controller/workspace.controller.js";
 
 const workspaceRouter = express.Router()
 
 workspaceRouter.post('/create', createWorkSpace)
+workspaceRouter.get('/workspaces',displayAllWorkspace)
 
 export default workspaceRouter
